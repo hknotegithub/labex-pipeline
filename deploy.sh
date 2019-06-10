aebc60814f01118ff18e30703b7b10df09a4b42c
andi1991 <lwjydcg@163.com>
2019-06-10 17:25:40 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+jdk="jdk1.8.0_181"
+tomcat="apache-tomcat-8.5.41"
+
+cd /root/
+
+echo "download jdk\n"
+rm -f ${jdk}.tar.gz
+wget https://labex-ali-data.oss-us-west-1.aliyuncs.com/spark-analysis/jdk-8u181-linux-x64.tar.gz -O ${jdk}.tar.gz
+
+echo "download tomcat\n"
+rm -f ${tomcat}.tar.gz
+wget https://labex-ali-data.oss-us-west-1.aliyuncs.com/jenkins/${tomcat}.tar.gz
+
+echo "install jdk\n"
+rm -rf /usr/local/${jdk}
+tar -zxf ${jdk}.tar.gz -C /usr/local/
+
+echo "install tomcat\n"
+rm -rf /usr/local/${tomcat}
+tar -zxf ${tomcat}.tar.gz -C /usr/local/
+
+echo "modify environment variable\n"
+sed -i '/JAVA_HOME/d' /etc/profile
+echo "export JAVA_HOME=/usr/local/${jdk}" >> /etc/profile
+echo 'export PATH=$JAVA_HOME/bin:$PATH'  >> /etc/profile
+source /etc/profile
+
+echo "start tomcat\n"
+if [ -e /usr/local/${tomcat}/bin/shutdown.sh ]; then
+	sh /usr/local/${tomcat}/bin/shutdown.sh
+fi
+sh /usr/local/${tomcat}/bin/startup.sh 
+
+echo "success"
